a8b5b7f7f2d81d1e0ae474a252ff92d16a309f19
arbaz <sayyedarbaz956@example.com>
2025-01-21 23:46:22 +0530

    nav fixed

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,17 @@
 import { Home, About, SpecialPackages, Gallery, InteriorProjectTour, Contact } from './navElements';
-import { BrowserRouter as NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './nav.css';
 function nav() {
     return (
         <nav className="foreground">
             <ul id="list" type="none">
-                 <button className='btn' id="home"> <NavLink to="/home"><Home /></NavLink></button>
-                 <button className='btn' id="about"> <NavLink to="/about"><About /></NavLink> </button>
-                 <button className='btn' id="sp"> <NavLink to="/specialPackages"><SpecialPackages /></NavLink> </button>
-                 <button className='btn' id="gall"> <NavLink to="/gallery"><Gallery /></NavLink> </button>
-                 <button className='btn' id="ipt"> <NavLink to="/interiorProjectTour"><InteriorProjectTour /></NavLink> </button>
-                 {/* <button className='btn' id="ib"> <NavLink to="/interiorBlog"><InteriorBlog /></NavLink> </button> */}
-                 <button className='btn' id="con"> <NavLink to="/contact"><Contact /></NavLink> </button>
+                 <button className='btn' id="home"> <Link to="/home"><Home /></Link></button>
+                 <button className='btn' id="about"> <Link to="/about"><About /></Link> </button>
+                 <button className='btn' id="sp"> <Link to="/specialPackages"><SpecialPackages /></Link> </button>
+                 <button className='btn' id="gall"> <Link to="/gallery"><Gallery /></Link> </button>
+                 <button className='btn' id="ipt"> <Link to="/interiorProjectTour"><InteriorProjectTour /></Link> </button>
+                 {/* <button className='btn' id="ib"> <Link to="/interiorBlog"><InteriorBlog /></Link> </button> */}
+                 <button className='btn' id="con"> <Link to="/contact"><Contact /></Link> </button>
             </ul>
         </nav>
     );
